508d7fe9d5eee73a07aae4dba14c3a68d4fecc9e
Ella Rose <erose1337@hotmail.com>
2016-06-16 15:46:22 -0700

    fixed streamcipher bug

--- streamcipher.c
+++ streamcipher.c
@@ -5,12 +5,12 @@
 #define WORDSIZE unsigned char
 #define WORDSIZE_BITS 8
 #define ROTATIONS 5
-#define PRINT_STRING "%i: %i\n"
+#define PRINT_STRING "%u: %u\n"
 
 #ifndef memcpy_s
 void memcpy_s(WORDSIZE* s1, WORDSIZE* s2, size_t n)
 {
-    int index;
+    unsigned int index;
     for (index = 0; index < n; index++)
     {
         s1[index] = s2[index];
@@ -33,25 +33,10 @@ WORDSIZE rotate_left(WORDSIZE word, int amount)
     return ((word << amount) | (word >> (WORDSIZE_BITS - amount)));
 }
 
-WORDSIZE bit_mixing(WORDSIZE* data, int start, int direction, int size)
-{
-    int index, counter, next_index;
-    WORDSIZE key;
-    index = start;
-    key = 0;
-    for (counter = 0; counter < size; counter++)
-    {    
-        next_index = (index + 1) % size;
-        data[next_index] ^= rotate_left(data[index], ((index + index + 1) % WORDSIZE_BITS)); 
-        key ^= data[next_index];
-        index += direction;
-    }
-    return key;
-}
-
-void shuffle_bytes(WORDSIZE* _state)
+WORDSIZE shuffle_bytes(WORDSIZE* _state)
 {
     WORDSIZE temp[16];
+    WORDSIZE key = 0, index, data_byte;
     
     temp[7]  = _state[0];
     temp[12] = _state[1];
@@ -70,33 +55,49 @@ void shuffle_bytes(WORDSIZE* _state)
     temp[10] = _state[14];
     temp[3]  = _state[15];
     
-    memcpy_s(_state, temp, 16);   
+    for (index = 0; index < 16; index++)
+    {
+        data_byte = temp[index];
+        key ^= data_byte;
+        _state[index] = data_byte;
+    }
+    //memcpy_s(_state, temp, 16);   
+    return key;    
+    
 }    
     
-WORDSIZE decorrelation_layer(WORDSIZE* data, int data_size)
-{    
-    WORDSIZE key;
-    key = bit_mixing(data, 0, 1, data_size);
-    shuffle_bytes(data);
-    return key;
-}
-
 int prp(WORDSIZE* data, WORDSIZE data_size)
 {
-    WORDSIZE index, data_byte, key;
-    key = decorrelation_layer(data, data_size);
+    WORDSIZE index, _index, left, right, key;    
+    key = shuffle_bytes(data);
+    
+    key ^= data[0];
+    data[0] = data[0] + key;
+    key ^= data[0];
     
     for (index = 0; index < data_size; index++)
     {    
-        data_byte = data[index];
-        key ^= data_byte;                       
-        data[index] = rotate_left((data_byte + key + index), ROTATIONS);        
-        key ^= data[index]; 
+        _index = (index + 1) % 16;
+        
+        right = data[_index];
+        
+        key ^= right;
+        right = rotate_left((right + key + index), ROTATIONS);
+        key ^= right;
+        
+        data[_index] = right;        
+        left = data[index];
+        
+        key ^= left;
+        left = (left + (right >> (WORDSIZE_BITS / 2)));
+        left ^= rotate_left(right, ROTATIONS);
+        key ^= left;
+        data[index] = left;
     }
     return key;
 }
         
-int prf(WORDSIZE* data, WORDSIZE key, WORDSIZE data_size)
+void prf(WORDSIZE* data, WORDSIZE key, WORDSIZE data_size)
 {
     WORDSIZE index, byte;
     for (index = 0; index < data_size; index++)
@@ -107,18 +108,16 @@ int prf(WORDSIZE* data, WORDSIZE key, WORDSIZE data_size)
     }
 }
     
-WORDSIZE xor_with_key(WORDSIZE* data, WORDSIZE* key)
+void xor_with_key(WORDSIZE* data, WORDSIZE* key)
 {
-    WORDSIZE data_xor = 0, index;
+    WORDSIZE index;
     for (index = 0; index < 16; index++)
     {           
-        data[index] ^= key[index];
-        data_xor ^= data[index];
-    }    
-    return data_xor;
+        data[index] ^= key[index];        
+    }        
 }
 
-void stream_cipher(WORDSIZE* data, WORDSIZE* _seed, WORDSIZE* _key, WORDSIZE* output, unsigned long blocks)
+void stream_cipher(WORDSIZE* data, WORDSIZE* _seed, WORDSIZE* _key, unsigned long blocks)
 {
     WORDSIZE key[16], round_key[16], seed[16];    
     WORDSIZE key_xor = 0, data_xor = 0;
@@ -135,8 +134,9 @@ void stream_cipher(WORDSIZE* data, WORDSIZE* _seed, WORDSIZE* _key, WORDSIZE* ou
         memcpy_s(round_key, key, 16);
         prf(round_key, key_xor, 16);        
             
-        data_xor = xor_with_key(seed, round_key);                      
-        prf(seed, data_xor, 16); 
+        xor_with_key(seed, round_key);                      
+        data_xor = prp(seed, 16); 
+        prf(seed, data_xor, 16);
         xor_with_key(seed, round_key);
                
         
@@ -145,17 +145,15 @@ void stream_cipher(WORDSIZE* data, WORDSIZE* _seed, WORDSIZE* _key, WORDSIZE* ou
 }
   
 void encrypt(WORDSIZE* data, WORDSIZE* key, WORDSIZE* seed, unsigned long data_size)
-{
-    WORDSIZE* key_material;
-    unsigned long blocks, extra;
-    key_material = malloc(data_size);
+{    
+    unsigned long blocks, extra;    
     blocks = data_size / 16;
     extra = data_size % 16;
     if (extra)
     {
         blocks += 1;
     }
-    stream_cipher(data, seed, key, key_material, blocks);
+    stream_cipher(data, seed, key, blocks);
 }
 
 void decrypt(WORDSIZE* data, WORDSIZE* key, WORDSIZE* seed, unsigned long data_size)
@@ -169,13 +167,16 @@ void test_encrypt_decrypt()
     
     memset(null_string, 0, 16);
     memcpy_s(data, null_string, 16);       
-    data[15] = 1;
+    //data[15] = 1;
     memcpy_s(plaintext, data, 16);
     memcpy_s(key, null_string, 16); 
     memcpy_s(seed, null_string, 16);
     
-    printf("Plaintext:\n");
-    print_data(data);
+    //WORDSIZE xor;
+    //xor = prp(data, 16);
+    //prf(data, xor, 16);
+    //printf("Data:%u \n%s\n", xor, data);
+    //print_data(data);
     encrypt(data, key, seed, 16);
     
     printf("Ciphertext:\n %s\n", data);    
@@ -186,7 +187,8 @@ void test_encrypt_decrypt()
     print_data(data);
 }
 
-void main()
+int main()
 {
     test_encrypt_decrypt();    
+    return 0;
 }
