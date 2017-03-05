1ffbc6969807d1707ae61580138b6d2435206fdd
Ella Rose <python_pride@protonmail.com>
2017-03-04 23:22:30 -0800

    added strange hash idea

--- designs/hash/strangehash.py
+++ designs/hash/strangehash.py
@@ -1,19 +1,31 @@
-def branch(word):
+from crypto.utilities import rotate_left, pad_input, slide
+
+def branch(word): # branch == 5
     word ^= rotate_left(word, 3, 8)
     word ^= rotate_left(word, 6, 8)
     return word
     
-def strange_hash(data):
-    output = 0
-    key = sorted(set([branch(word) for word in range(256)]))
-    for index, byte in enumerate(data):
-        output ^= key[branch(byte ^ output ^ index)]
-    return output
+def strange_hash(data): # outputs span a 5-bit range; the hash of the first 32 integers yields the 32 outputs the hash can produce; 
+    output = bytearray()
+    state = 0
+    key = list(set([branch(word) for word in range(256)]))
+    key = dict((entry, key.index(entry)) for entry in key)
+
+    data = bytearray(pad_input(data, 1))
+    for index, byte in enumerate(data):        
+        state ^= key[branch(byte ^ index)]
+        output.append(state)        
+    return quints_to_bytes(output)
     
-def test_strange_hash():
-    for byte in range(256):
-        print strange_hash(chr(byte))
+def quints_to_bytes(hash_output): 
+    bits = ''.join(format(word, 'b').zfill(5) for word in hash_output)
+    return bytearray(int(_bits, 2) for _bits in slide(bits, 8))                    
         
+def test_strange_hash():
+    outputs = []
+    for byte in range(256):
+        outputs.append(bytes(strange_hash(chr(byte))))
+    print '\n'.join(bytes(item) for item in outputs[:64])
         
 if __name__ == "__main__":
     test_strange_hash()
---
+++ designs/hash/strangehash2.py
@@ -0,0 +1,37 @@
+from crypto.utilities import rotate_left, pad_input, slide, bytes_to_words, words_to_bytes
+
+def branch(word):             
+    # 0 1 2 3  4 5 6 7  8 9 10 11  12 13 14 15
+    word ^= rotate_left(word, 3, 16)
+    word ^= rotate_left(word, 6, 16)
+    word ^= rotate_left(word, 5, 16)
+    return word
+    
+KEY = list(set([branch(word) for word in range(65536)]))
+KEY = dict((entry, KEY.index(entry)) for entry in KEY)
+        
+def strange_hash(data, key=KEY): 
+    output = list()
+    state = 0
+    data = bytes_to_words(bytearray(pad_input(data, 2)), 2)
+    for index, word in enumerate(data):        
+        state ^= key[branch(word ^ index)]        
+        output.append(state)        
+    return odd_size_to_bytes(output, 12)
+    
+def odd_size_to_bytes(hash_output, word_size): 
+    bits = ''.join(format(word, 'b').zfill(word_size) for word in hash_output)
+    return bytearray(int(_bits, 2) for _bits in slide(bits, 8))
+        
+def test_strange_hash():
+    outputs = []
+    for byte in range(256):
+        for byte2 in range(256):
+            outputs.append(bytes(strange_hash(chr(byte) + chr(byte2))))
+        #outputs.append(bytes(strange_hash(chr(byte))))
+    #print '\n'.join(bytes(item) for item in outputs[:64])
+    print len(set(outputs[4096 * 2:4096 * 3]))
+    
+if __name__ == "__main__":
+    test_strange_hash()
+    
