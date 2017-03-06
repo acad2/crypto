6f0317b6954959941fb1d81f1e5b779cd9880fe1
Ella Rose <python_pride@protonmail.com>
2017-03-05 18:25:26 -0800

    added inverse search

--- designs/blockcipher/strangeencryptor.py
+++ designs/blockcipher/strangeencryptor.py
@@ -11,7 +11,16 @@ def branch(word):
     # 0136 1247 0235 1346 2457 0356 1467 0257
     
     # 0136 1247 0235 1346 2457 0356 1467 0257
-    # 1346 2457 0356 1467 0257 0136 1247 0235 
+    # 1247 0235 1346 2457 0356 1467 0257 0136 # rotate left by 1
+    # 023467 013457 012456 123567 023467 012456 123567
+    
+    # 0136 1247 0235 1346 2457 0356 1467 0257
+    # 0235 1346 2457 0356 1467 0257 0136 1247 
+    
+    
+    
+    # 0136 1247 0235 1346 2457 0356 1467 0257
+    # 1346 2457 0356 1467 0257 0136 1247 0235  # rotate left by 4
     # 04 15 26 37 04 15 26 37
             
     word ^= rotate_left(word, 3, 8)
@@ -25,24 +34,57 @@ _KEY = dict((value, key) for key, value in KEY.items())
 def encrypt(data, key, decoder=KEY, rounds=ROUNDS):
     key <<= 5
     for round in range(rounds):
+        print "Data: ", data
         data ^= key
-        data = decoder[branch(data)]
+        print "Data ^ key: ", data
+        data = branch(data)
+        print "Branch(data): ", data
+        data = decoder[data]
+        print "Decoder[data]: ", data
     return data
     
 def decrypt(data, key, encoder=_KEY, rounds=ROUNDS):
     key <<= 5
     for round in range(rounds):
-        data = unbranch(encoder[data])
+        print "Data: ", data
+        data = encoder[data]
+        print "Encoded data: ", data
         data ^= key               
-        
+        print "Data ^ key: ", data
+    return data
+    
 def test_encrypt():
     ciphertexts = []
     key = 0
     for byte in range(32):
         ciphertext = encrypt(byte, key)
+        plaintext = decrypt(ciphertext, key)
+        assert plaintext == byte, (plaintext, byte)        
         ciphertexts.append(ciphertext)
     print len(set(ciphertexts)), ciphertexts
     
+def search_for_inverse_branch(): # not so simple, unfortunately; no valid inverses produced this way.
+    solutions = []
+    for byte in range(32):
+        branched = branch(byte)
+        for r1 in range(8):
+            for r2 in range(8):
+                def inverse_branch(word):
+                    word ^= rotate_left(word, r1, 8)
+                    word ^= rotate_left(word, r2, 8)
+                    return word
+                if inverse_branch(branched) == byte:
+                    solutions.append((r1, r2, inverse_branch))
+    for r1, r2, inverse in set(solutions):
+        for byte in range(32):
+            branched = branch(byte)
+            if inverse(branched) != byte:
+                print "Invalid inverse: ", r1, r2, byte
+                break
+        else:
+            print "Valid inverse: ", r1, r2
+    
 if __name__ == "__main__":
     test_encrypt()
+    #search_for_inverse_branch()
     
