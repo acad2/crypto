96d8a4fb27534a97aa4df26ec3315d7b48532e2a
Ella Rose <python_pride@protonmail.com>
2017-04-26 12:49:02 -0700

    attempted to fix the problem again...

--- C/42517/hash.c
+++ C/42517/hash.c
@@ -8,7 +8,7 @@
     
 void hash_function(WORDSIZE* hash_input, WORDSIZE input_length, WORDSIZE* output){
     WORDSIZE state[16] __attribute__((aligned(16)));
-    unsigned long index, block_number, number_of_blocks;
+    unsigned long index, block_number = 0, number_of_blocks;
     
     zero_out(state, 16);
                 
@@ -16,11 +16,10 @@ void hash_function(WORDSIZE* hash_input, WORDSIZE input_length, WORDSIZE* output
     if (input_length % 8 == 0){
         if (number_of_blocks >= 1){
             number_of_blocks -= 1;}}
-    
-    for (block_number = 0; block_number < number_of_blocks; block_number++){    
-        absorb(state, hash_input, 8, (block_number * 8), block_number);}
-    
-    block_number += 1;    
+    if (number_of_blocks > 0){        
+        for (block_number = 0; block_number < number_of_blocks; block_number++){        
+            absorb(state, hash_input, 8, (block_number * 8), block_number);}}
+        
     int amount = input_length % 8;
     if (amount == 0){
         amount = 8;}
@@ -36,7 +35,7 @@ void test_hash(){
     
     zero_out(message, message_size);
     clock_t begin = clock();
-    for (index = 0; index < 2; index++){
+    for (index = 0; index < 3000000; index++){
         hash_function(message, message_size, hash_result);}
     clock_t end = clock();
     
@@ -44,7 +43,7 @@ void test_hash(){
     printf("Time required: %.2fs\n", time_spent);
     printf("%lu%lu%lu%lu%lu%lu%lu%lu\n", hash_result[0], hash_result[1], hash_result[2], hash_result[3], hash_result[4], hash_result[5], hash_result[6], hash_result[7]);}
     
-int main(){
+/*int main(){
     test_hash();
-    return 0;}
+    return 0;}*/
     
