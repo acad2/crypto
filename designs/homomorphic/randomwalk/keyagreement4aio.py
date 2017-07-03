41922f523063e342778afd6773b46c6aebb2b82e
Ella Rose <python_pride@protonmail.com>
2017-07-02 17:18:14 -0700

    made aio file

--- designs/homomorphic/randomwalk/keyagreement4aio.py
+++ designs/homomorphic/randomwalk/keyagreement4aio.py
@@ -33,8 +33,82 @@
 #a1(a2x + b2) + b1
 #a1a2x + a1b2 + b1
 
-from crypto.utilities import random_integer, big_prime, modular_inverse, modular_subtraction
+from crypto.utilities import  modular_inverse, modular_subtraction
 
+# utilities
+def bytes_to_integer(data):
+    output = 0    
+    size = len(data)
+    for index in range(size):
+        output |= data[index] << (8 * (size - 1 - index))
+    return output
+    
+def random_integer(size_in_bytes):
+    return bytes_to_integer(bytearray(random_bytes(size_in_bytes)))
+       
+def big_prime(size_in_bytes):           
+    while True:
+        candidate = random_integer(size_in_bytes)
+        if candidate > 1 and is_prime(candidate):
+            return candidate
+
+def is_prime(n, _mrpt_num_trials=10): # from https://rosettacode.org/wiki/Miller%E2%80%93Rabin_primality_test#Python
+    assert n >= 2
+    # special case 2
+    if n == 2:
+        return True
+    # ensure n is odd
+    if n % 2 == 0:
+        return False
+    # write n-1 as 2**s * d
+    # repeatedly try to divide n-1 by 2
+    s = 0
+    d = n-1
+    while True:
+        quotient, remainder = divmod(d, 2)
+        if remainder == 1:
+            break
+        s += 1
+        d = quotient
+    assert(2**s * d == n-1)
+ 
+    # test the base a to see whether it is a witness for the compositeness of n
+    def try_composite(a):
+        if pow(a, d, n) == 1:
+            return False
+        for i in range(s):
+            if pow(a, 2**i * d, n) == n-1:
+                return False
+        return True # n is definitely composite
+    
+    random.seed(random_bytes(32))
+    for i in range(_mrpt_num_trials):
+        a = random.randrange(2, n)
+        if try_composite(a):
+            return False
+ 
+    return True # no base tested showed n as composite
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modular_inverse(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise ValueError('modular inverse does not exist')
+    else:
+        return x % m
+
+def modular_subtraction(x, y, modulus):
+    return (modulus + (x - y)) % modulus        
+    
+# end utilities
+
+# parameter generation
 A_SIZE = 32
 B_SIZE = 32
 X_SIZE = 32
@@ -52,6 +126,9 @@ def generate_parameters(a_size=A_SIZE, b_size=B_SIZE,
     
 PARAMETERS = A, B, X, P, Z = generate_parameters()        
     
+# end parameter generation
+
+# algorithm     
 def point_addition(x, a, b, p=P):  
     return ((a * x) + b) % p
            
@@ -81,6 +158,9 @@ def key_agreement(public_key, private_key, p=P):
     _a, _b = private_key
     return point_addition(public_key, _a, _b, p)
     
+# end algorithm
+
+# testing    
 def test_key_agreement():
     from unittesting import test_key_agreement
     test_key_agreement("keyagreement4", generate_keypair, key_agreement, iterations=10000)
