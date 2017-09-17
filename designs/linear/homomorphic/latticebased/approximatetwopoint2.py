7c164853d37fbf8a9f794ff20b40a9be81874fd9
Ella Rose <python_pride@protonmail.com>
2017-09-17 15:31:56 -0700

    added non-square variant

--- designs/linear/homomorphic/latticebased/approximatetwopoint2.py
+++ designs/linear/homomorphic/latticebased/approximatetwopoint2.py
@@ -17,17 +17,18 @@ def generate_private_key(security_level=SECURITY_LEVEL):
         
         Returns the integer(s) that constitute a private key. """
     s = random_integer(security_level)
-    return s
+    s2 = random_integer(security_level * 2)
+    return s, s2
     
 def generate_public_key(private_key, points=POINTS, security_level=SECURITY_LEVEL):
     """ usage: generate_public_key(private_key, points=POINTS, 
                                    security_level=SECURITY_LEVEL) => public_key
                                    
         Returns the integer(s) that constitute a public key. """
-    s = private_key
+    s, s2 = private_key
     e = random_integer(security_level + (security_level / 2))
     a, b = points
-    return ((a * (s ** 2)) + (b * s) + e)
+    return (a * s2) + (b * s) + e
     
 def generate_keypair(security_level=SECURITY_LEVEL, points=POINTS):
     """ usage: generate_keypair(security_level=SECURITY_LEVEL, points=POINTS) => public_key, private_key
@@ -37,13 +38,13 @@ def generate_keypair(security_level=SECURITY_LEVEL, points=POINTS):
     public_key = generate_public_key(private_key, points, security_level)
     return public_key, private_key
     
-def key_agreement(public_key, private_key, shift=930):
-    """ usage: key_agreement(public_key, private_key, shift=930):
+def key_agreement(public_key, private_key, shift=920):
+    """ usage: key_agreement(public_key, private_key, shift=910):
         
         Returns a shared secret in the form of an integer.
         The shift argument must be modified if the security_level has been altered from the default. """
-    s = private_key
-    approximate_shared_point = ((s ** 2) * public_key)
+    s, s2 = private_key
+    approximate_shared_point = (s2 * public_key)
     output = approximate_shared_point >> shift
     if not output:
         raise ValueError("Shared secret was 0")
