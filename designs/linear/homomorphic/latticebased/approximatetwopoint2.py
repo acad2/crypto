5b34ba01db3881d42659458fbaa45cdb330afc89
Ella Rose <python_pride@protonmail.com>
2017-09-17 15:34:41 -0700

    simplified code

--- designs/linear/homomorphic/latticebased/approximatetwopoint2.py
+++ designs/linear/homomorphic/latticebased/approximatetwopoint2.py
@@ -15,20 +15,20 @@ POINTS = [random_integer(SECURITY_LEVEL / 2) for count in range(2)]
 def generate_private_key(security_level=SECURITY_LEVEL):
     """ usage: generate_private_key(security_level=SECURITY_LEVEL) => private_key
         
-        Returns the integer(s) that constitute a private key. """
-    s = random_integer(security_level)
-    s2 = random_integer(security_level * 2)
-    return s, s2
+        Returns the integer(s) that constitute a private key. """    
+    s = random_integer(security_level * 2)
+    return s
     
 def generate_public_key(private_key, points=POINTS, security_level=SECURITY_LEVEL):
     """ usage: generate_public_key(private_key, points=POINTS, 
                                    security_level=SECURITY_LEVEL) => public_key
                                    
         Returns the integer(s) that constitute a public key. """
-    s, s2 = private_key
+    s = private_key
+    s2 = random_integer(security_level)
     e = random_integer(security_level + (security_level / 2))
     a, b = points
-    return (a * s2) + (b * s) + e
+    return (a * s) + (b * s2) + e
     
 def generate_keypair(security_level=SECURITY_LEVEL, points=POINTS):
     """ usage: generate_keypair(security_level=SECURITY_LEVEL, points=POINTS) => public_key, private_key
@@ -43,8 +43,8 @@ def key_agreement(public_key, private_key, shift=920):
         
         Returns a shared secret in the form of an integer.
         The shift argument must be modified if the security_level has been altered from the default. """
-    s, s2 = private_key
-    approximate_shared_point = (s2 * public_key)
+    s = private_key
+    approximate_shared_point = (s * public_key)
     output = approximate_shared_point >> shift
     if not output:
         raise ValueError("Shared secret was 0")
