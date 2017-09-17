3dcd80e847189a842975f1395c18473ff570ae70
Ella Rose <python_pride@protonmail.com>
2017-09-16 20:47:41 -0700

    fixed bug

--- designs/linear/homomorphic/latticebased/approximatesquared.py
+++ designs/linear/homomorphic/latticebased/approximatesquared.py
@@ -1,11 +1,12 @@
-# axx + bx + y
-# ass + bs + e
-
-#ss(axx + bx + y)
-#xx(ass + bs + e)
-
-#ssaxx + ssbx + ssy
-#xxass + xxbs + xxe
+# public parameters:
+#   integers a, b
+# private key:
+#   integer x
+# public key:
+#   axx + bx + y
+# (approximate) key agreement:
+#   ss(axx + bx + y) = axxss + bxss + yss
+#   xx(ass + bs + e) = axxss + bxxs + exx
 from crypto.utilities import random_integer
 
 SECURITY_LEVEL = 32
@@ -19,17 +20,20 @@ def generate_public_key(private_key, points=POINTS, security_level=SECURITY_LEVE
     s = private_key
     e = random_integer(security_level * 2)
     a, b = points
-    return ((a * (s ** 2)) + (b * 2) + e)
+    return ((a * (s ** 2)) + (b * s) + e)
     
 def generate_keypair(security_level=SECURITY_LEVEL, points=POINTS):
     private_key = generate_private_key(security_level)
     public_key = generate_public_key(private_key, points, security_level)
     return public_key, private_key
     
-def key_agreement(public_key, private_key, shift=1038):
+def key_agreement(public_key, private_key, shift=1043):
     s = private_key
-    approximate_shared_point = (s ** 2) * public_key
-    return approximate_shared_point >> shift
+    approximate_shared_point = ((s ** 2) * public_key)
+    output = approximate_shared_point >> shift
+    if not output:
+        raise ValueError("Shared secret was 0")
+    return output
     
 def test_key_agreement():
     from unittesting import test_key_agreement
