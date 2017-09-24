ceffd39a702b2931c044536c090cc832cbe762e5
Ella Rose <python_pride@protonmail.com>
2017-09-23 18:48:44 -0700

    syncing

--- designs/linear/homomorphic/latticebased/keyagreement12.py
+++ designs/linear/homomorphic/latticebased/keyagreement12.py
@@ -1,12 +1,15 @@
-# as + (y << n) == as + y*(2**n)
-# 32 32   32 32    64  64
+# as + (y << n) == as + y*(2**n)    32 32   32 32    64  64
+# e || as
+#x(e || as) == xe  asx     32 32     32 32 32   96  
+#s(y || ax) == sy  asx
+
 from crypto.utilities import random_integer, big_prime
 
 SECURITY_LEVEL = 32
 SECURITY_LEVEL_BITS = 32 * 8
 A = random_integer(SECURITY_LEVEL)
 N = 2 ** (SECURITY_LEVEL_BITS)
-P = big_prime(SECURITY_LEVEL + 1)
+P = big_prime((SECURITY_LEVEL * 2))
 
 def generate_private_key(security_level=SECURITY_LEVEL):
     s = random_integer(security_level)
@@ -15,15 +18,15 @@ def generate_private_key(security_level=SECURITY_LEVEL):
 def generate_public_key(private_key, security_level=SECURITY_LEVEL, a=A, shift=SECURITY_LEVEL_BITS):
     s = private_key
     e = random_integer(security_level)
-    return (a * s) + (e << shift)
+    return ((a * s) + (e << shift)) % P
     
 def generate_keypair(security_level=SECURITY_LEVEL, a=A, shift=SECURITY_LEVEL_BITS):
     private_key = generate_private_key(security_level)
     public_key = generate_public_key(private_key, security_level, a, shift)
     return public_key, private_key
     
-def key_agreement(public_key, private_key, mask=N - 1):
-    return (public_key * private_key) & mask
+def key_agreement(public_key, private_key, mask=(N >> 224) - 1):
+    return ((public_key * private_key) % P) & mask
     
 def unit_test():
     from unittesting import test_key_agreement
