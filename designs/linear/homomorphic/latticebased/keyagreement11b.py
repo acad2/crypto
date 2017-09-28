63b94af5825a6bc4b032dbbd95bac96021dbe1c0
Ella Rose <python_pride@protonmail.com>
2017-09-27 23:20:38 -0700

    fixed shift amount

--- designs/linear/homomorphic/latticebased/keyagreement11b.py
+++ designs/linear/homomorphic/latticebased/keyagreement11b.py
@@ -9,42 +9,49 @@
 #                   6  6  6    6  6  2    6  6         18  14 12
 from crypto.utilities import random_integer
 
-SIZE_DIFFERENCE = 1
+SIZE_DIFFERENCE = 8
 SECURITY_LEVEL = 32
+PADDING_AMOUNT = 2
 
-def generate_parameters(security_level, size_difference):         
+def generate_parameters(security_level, size_difference, padding):         
     scaler = security_level / size_difference
     e_size = (security_level - size_difference) * scaler    
     a_size = s_size = r_size = security_level * scaler
-    shift_amount = (((a_size + s_size + s_size + 4) - (security_level)) * 8)
+    
+    shift_amount = ((a_size * 3) - (security_level - padding)) * 8  
     return e_size, s_size, a_size, shift_amount, r_size
     
-E_SIZE, S_SIZE, A_SIZE, SHIFT, ERROR_SIZE = generate_parameters(SECURITY_LEVEL, SIZE_DIFFERENCE)
+E_SIZE, S_SIZE, A_SIZE, SHIFT, ERROR_SIZE = generate_parameters(SECURITY_LEVEL, SIZE_DIFFERENCE, PADDING_AMOUNT)
 A = random_integer(A_SIZE)
 
-def generate_private_key(security_level=SECURITY_LEVEL, s_size=S_SIZE, e_size=E_SIZE):    
- #   print A_SIZE, s_size, e_size, ERROR_SIZE, SHIFT
-    s = random_integer(s_size)
-    e = random_integer(e_size)
-    return s, e
+def generate_private_key(security_level=SECURITY_LEVEL, s_size=S_SIZE):    
+    s = random_integer(s_size)    
+    return s
     
-def generate_public_key(private_key, error_size=ERROR_SIZE, a=A):
-    s, e = private_key        
-    return (s * (a + e)) + random_integer(error_size)
+def generate_public_key(private_key, e_size=E_SIZE, error_size=ERROR_SIZE, a=A):
+    s = private_key        
+    e = random_integer(e_size)
+    r = random_integer(error_size)
+    return (s * (a + e)) + r
     
 def generate_keypair(security_level=SECURITY_LEVEL, a=A, s_size=S_SIZE, e_size=E_SIZE, error_size=ERROR_SIZE):
-    private_key = generate_private_key(security_level, s_size, e_size)
-    public_key = generate_public_key(private_key, error_size, a)
+    private_key = generate_private_key(security_level, s_size)
+    public_key = generate_public_key(private_key, e_size, error_size, a)
     return public_key, private_key
     
-def key_agreement(public_key, private_key, shift=SHIFT):   
-  #  from math import log
- #   print log(public_key * private_key[2], 2) - shift
-    return (public_key * private_key[0]) >> shift
+def key_agreement(public_key, private_key, shift=SHIFT): 
+    return (public_key * private_key) >> shift
     
 def unit_test():
+    public1, private1 = generate_keypair()
+    public2, private2 = generate_keypair()
+    share = key_agreement(public1, private2)
+    test = (public1 * public2)
+    def f(integer):
+        return int(''.join(char for char in reversed(format(integer, 'b'))), 2) & ((1 << 256) - 1)
+    assert f(share) != f(test), "Broken"        
     from unittesting import test_key_agreement
-    test_key_agreement("s(a + e) key agreement b", generate_keypair, key_agreement, iterations=10000, key_size=SECURITY_LEVEL)
+    test_key_agreement("s(a + e) key agreement b", generate_keypair, key_agreement, iterations=100000, key_size=SECURITY_LEVEL)
     
 if __name__ == "__main__":
     unit_test()
