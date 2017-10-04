6e9e8ee3e1d2a5773f36a3ac13c24205333e3195
Ella Rose <python_pride@protonmail.com>
2017-10-03 21:39:24 -0700

    extended unit test

--- designs/linear/homomorphic/latticebased/manyinverses3.py
+++ designs/linear/homomorphic/latticebased/manyinverses3.py
@@ -1,11 +1,11 @@
-# a, a, a... (modular_inverse(ai, q * random_integer()))
-#ax + ay + az
+# a + a, a + a, a + a... (modular_inverse(ai, q * random_integer()))
+#ax + ax + ay + ay + az + az
 #ai(ax + ay + az)
-#x + y + z 
+#2(x + y + z)
 from crypto.utilities import random_integer, modular_inverse
 
 SECURITY_LEVEL = 32
-PUBLIC_KEY_SIZE = SECURITY_LEVEL
+PUBLIC_KEY_SIZE = 5#SECURITY_LEVEL
 
 def generate_private_key(security_level=SECURITY_LEVEL):
     while True:
@@ -21,10 +21,11 @@ def generate_private_key(security_level=SECURITY_LEVEL):
     
 def generate_public_key(private_key, security_level=SECURITY_LEVEL, public_key_size=PUBLIC_KEY_SIZE):
     ai, modulus = private_key
+    a = modular_inverse(ai, modulus)
     public_key = []
     while len(public_key) < public_key_size:        
         try:
-            public_key.append(modular_inverse(ai, modulus * random_integer(security_level)))
+            public_key.append(a + modular_inverse(ai, modulus * random_integer(security_level)))
         except ValueError:
             continue
     return public_key
@@ -41,26 +42,15 @@ def encapsulate_key(public_key, security_level=SECURITY_LEVEL):
         r = random_integer(security_level)
         ciphertext += entry * r
         secret += r        
-    return ciphertext, secret
+    return ciphertext, secret * 2
     
-def recover_key(ciphertext, private_key, depth=1):
+def recover_key(ciphertext, private_key):
     ai, modulus = private_key
     # ax + ay + az
     # x + y + z
-    return (ciphertext * pow(ai, depth, modulus)) % modulus
+    return (ciphertext * ai) % modulus
     
 def unit_test():
-    public1, private1 = generate_keypair()
-    c1, s1 = encapsulate_key(public1)
-    c2, s2 = encapsulate_key(public1)
-    c3, s3 = (c1 + c2), (s1 + s2)
-    c4, s4 = (c1 * c2), (s1 * s2) % private1[1]
-    
-    _s3 = recover_key(c3, private1)
-    _s4 = recover_key(c4, private1, depth=2)
-    assert _s3 == s3, (_s3, s3)
-    assert _s4 == s4, (_s4, s4)
-    
     from unittesting import test_key_exchange
     test_key_exchange("manyinverses_keyexchange", generate_keypair, encapsulate_key, recover_key, iterations=10000)
     
--- designs/linear/homomorphic/latticebased/shortypie.py
+++ designs/linear/homomorphic/latticebased/shortypie.py
@@ -1,9 +1,7 @@
-# a, ab
-#xa + yab    32 64   32 64
-# x + yb     32   64  
-#            96   64
-
-#            32 96   32 96
+# ax ay
+#sax + tay  32 96     32 96
+#sx + ty
+#s + xity   
 
 from crypto.utilities import random_integer, modular_inverse
 
@@ -25,8 +23,15 @@ def generate_private_key(security_level=SECURITY_LEVEL):
 def generate_public_key(private_key, security_level=SECURITY_LEVEL):
     ai, modulus = private_key
     a = modular_inverse(ai, modulus)
-    b = random_integer(security_level)
-    return a, (a * b) % modulus
+    x = random_integer(security_level)
+    y = (2 ** (security_level * 8))
+    return (a * x), (a * y) % modulus
     
 def generate_keypair(security_level=SECURITY_LEVEL):
+    private_key = generate_private_key(security_level)
+    public_key = generate_public_key(private_key, security_level)
+    return public_key, private_key
+    
+def encapsulate_key(public_key, security_level=SECURITY_LEVEL):
+    ax, ay = public_key
     
